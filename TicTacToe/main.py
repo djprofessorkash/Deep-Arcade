0c3d227d9bfc83b443699f500df2351704b067a2
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-01-06 20:20:15 -0500

    Constructing TicTacToe gameboard architecture

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -12,8 +12,31 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+class Tic_Tac_Toe_Environment(object):
+    """ Game board environment for Tic Tac Toe. """
+    def __init__(self, N=3, user_play_symbol="X"):
+        self.N = N
+        self.gameboard = None
+        self.reset_gameboard(N)
+        self.stale = False
+        self.syms_empty = {
+            "mark": " ",
+            "value": 0
+        }
+        self.syms_O = {
+            "mark": "O",
+            "value": 1
+        }
+        self.syms_X = {
+            "mark": "X",
+            "value": 2
+        }
+        self.player_sym, self.bot_sym = (self.syms_X, self.syms_O) if user_play_symbol.upper() == "X" else (self.syms_O, self.syms_X)
+        self.winner = None
+
 def main():
-    print("\nHELLO WORLD!\n")
+    """ Main run function. """
+    pass
 
 if __name__ == "__main__":
     main()
