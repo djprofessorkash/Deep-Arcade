893d0fed1c2d0fdb4cade7b97ce171b023ede513
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-01-07 14:59:17 -0500

    Finalized basic file architecture across Snake game

--- Snake/main.py
+++ Snake/main.py
@@ -4,3 +4,16 @@ AUTHOR:         Aakash Sudhakar
 
 SUMMARY:        Main Python file for controlling deep learning reinforcement simulation of Snake.
 """
+
+import pygame
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import pandas as pd
+# import numpy as np
+from structures import DeepQNetworkAgent, GameSession, PlayerInstance, PelletInstance
+
+def main():
+    print("\nHELLO WORLD\n")
+
+if __name__ == "__main__":
+    main()
---
+++ Snake/structures/DeepQNetworkAgent.py
@@ -0,0 +1,7 @@
+"""
+TITLE:          DeepQNetworkAgent.py (Snake)
+AUTHOR:         Aakash Sudhakar
+
+SUMMARY:        Support Python file holding the reinforcement learning algorithm for 
+                the deep learning reinforcement simulation of Snake.
+"""
