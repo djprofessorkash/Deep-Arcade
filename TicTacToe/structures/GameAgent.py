e68ec81d56e9371e9c7ae32465162b06a6ab851b
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-01-07 11:30:22 -0500

    Created state saving function for gameboard progress

--- TicTacToe/structures/GameAgent.py
+++ TicTacToe/structures/GameAgent.py
@@ -31,3 +31,8 @@ class TicTacToe_GameAgent(object):
         """ Method to teach reinforcement model to learn via temporal difference algorithm. """
         old_state = self.states.get(old_state_key, np.zeros((self.N, self.N)))
         return self.learning_rate * ((reward * self.states[new_state_key]) - old_state)
+
+    def set_state_by_action(self, old_gameboard, action):
+        """ Method to save action to given state across gameboard. """
+        state_key = TicTacToe_GameAgent.flatten_gameboard(old_gameboard)
+        self.state_order.append((state_key, action))
