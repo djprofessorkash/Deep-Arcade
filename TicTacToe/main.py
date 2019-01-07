f60b27ba69fae86bef2a5861870b1cab9002c70e
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-01-06 20:17:30 -0500

    Pushing hello-world boilerplate for TicTacToe game

---
+++ TicTacToe/main.py
@@ -0,0 +1,19 @@
+"""
+TITLE:          main.py (Tic Tac Toe)
+AUTHOR:         Aakash Sudhakar
+
+SUMMARY:        Main Python file for controlling deep learning reinforcement simulation 
+                of Tic Tac Toe.
+"""
+
+from matplotlib import pyplot as plt
+from matplotlib import ticker
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+def main():
+    print("\nHELLO WORLD!\n")
+
+if __name__ == "__main__":
+    main()
