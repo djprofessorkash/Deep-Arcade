5b26dde3d35086efea4c242d9c29c181dddcbde3
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-01-09 10:55:20 -0500

    Developed initializer and pellet instance methods for Snake food class

--- Snake/structures/PelletInstance.py
+++ Snake/structures/PelletInstance.py
@@ -5,3 +5,22 @@ AUTHOR:         Aakash Sudhakar
 SUMMARY:        Support Python file holding the pellet object instance for 
                 the deep learning reinforcement simulation of Snake.
 """
+
+from random import randint
+import pygame
+
+class PelletInstance(object):
+    """ Object storing pellet instance to serve as player food. """
+    def __init__(self):
+        self.dim_x = 240
+        self.dim_y = 200
+        pygame.image.load("img/pellet2.png")
+
+    def get_pellet_position(self, game_session, player_instance):
+        """ Method to grab current pellet position and check if pellet has been consumed by player. """
+        rand_x, rand_y = randint(20, game_session.play_width - 40), randint(20, game_session.play_height - 40)
+        self.dim_x, self.dim_y = rand_x - (rand_x % 20), rand_y - (rand_y % 20)
+        if [self.dim_x, self.dim_y] not in player_instance.position:
+            return self.dim_x, self.dim_y
+        else:
+            self.get_pellet_position(game_session, player_instance)
